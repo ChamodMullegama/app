ada415a4804beb6c898e99491d97159973724316
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2024-08-05 00:32:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,9 @@ console.log("hello world")
 console.log("hello world")
 console.log("hello world")
 
+console.log("chamod mullegama ")
+console.log("hello world")
+
 console.log("hello world")
 console.log("hello world")
 console.log("hello world")
